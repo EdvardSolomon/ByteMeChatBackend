e34e1911fa4031cd3bc12ddbfb42f01fa2916e1c
EdvardSolomon <60151581+EdvardSolomon@users.noreply.github.com>
2024-03-16 19:52:18 +0200

    Update artifact-build-deploy.sh

--- scripts/github/artifact-build-deploy.sh
+++ scripts/github/artifact-build-deploy.sh
@@ -13,3 +13,8 @@ done
 sed -i "s|%ECS_TASK_DEFINITION%|$ECS_TASK_DEFINITION|g" ecs-task-definition.json
 sed -i "s|%REGION%|$REGION|g" ecs-task-definition.json
 sed -i "s|%CONTAINER_NAME%|$CONTAINER_NAME|g" ecs-task-definition.json
+sed -i "s|%DB_NAME%|$DB_NAME|g" ecs-task-definition.json
+sed -i "s|%DB_HOST%|$DB_HOST|g" ecs-task-definition.json
+sed -i "s|%DB_PORT%|$DB_PORT|g" ecs-task-definition.json
+sed -i "s|%DB_USER%|$DB_USER|g" ecs-task-definition.json
+sed -i "s|%DB_PASSWORD%|$DB_PASSWORD|g" ecs-task-definition.json
